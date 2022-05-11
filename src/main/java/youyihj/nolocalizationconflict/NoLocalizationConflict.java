f48cf5681600c131c5dcfef01a731e33ce93c7c0
youyihj <youyi580@qq.com>
2022-05-11 17:53:45 +0800

    version 1.2. Close #2

--- src/main/java/youyihj/nolocalizationconflict/NoLocalizationConflict.java
+++ src/main/java/youyihj/nolocalizationconflict/NoLocalizationConflict.java
@@ -21,7 +21,7 @@ import java.util.Optional;
 public class NoLocalizationConflict {
     public static final String MODID = "nolocalizationconflict";
     public static final String NAME = "No Localization Conflict";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
     public static final String DEPENDENCIES = "required-after:mixinbooter@[4.2,)";
     public static final Map<URL, ModContainer> pathToModMap = new HashMap<>();
     public static Logger logger;
